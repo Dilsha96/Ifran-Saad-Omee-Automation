10393684c39d1eafd8f576c5b5c70141730eddad
dilsha96 <dilshaishara123@gmail.com>
2025-07-04 21:12:03 +0530

    Project Initiated.
    New project has been create.

---
+++ ui-automation/src/main/java/Ifran_Saad_Omee/ui_automation/App.java
@@ -0,0 +1,13 @@
+package Ifran_Saad_Omee.ui_automation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
